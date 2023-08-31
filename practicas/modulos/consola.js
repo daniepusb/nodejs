d056f9294fd87db728525947620847cbd2c9cbc7
Daniel <danie.p.usb@gmail.com>
2023-08-31 05:33:57 +0200

    probando las deiferentes formas de console.log

---
+++ practicas/modulos/consola.js
@@ -0,0 +1,19 @@
+/* console.log('ejemplo');
+console.info('ejemplo');
+console.warn('ejemplo'); */
+
+let tablita = [{a:1,b:'Z'},{a:2,b:'Otra letra'}]
+
+console.table(tablita);
+
+console.group('grupo_uno');
+console.log('identado');
+console.log('identado');
+console.log('identado');
+console.groupEnd('grupo_uno');
+
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
